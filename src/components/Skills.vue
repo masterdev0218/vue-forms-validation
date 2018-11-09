5067f9507af0db536bebd763a6698a6c0677b738
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-09 11:59:01 +0100

    add router, about page and transitioning css

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="container">
 		<div class="holder">
+			<form @submit.prevent="addSkill">
+				<input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
 
-			<input type="text" placeholder="Enter a skill you have.." v-model="skill">
-			{{ skill }}
+				<transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+					<p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+				</transition>
+			</form>
+
 			<ul>
-				<li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+				<transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+					<li v-for="(data, index) in skills" :key='index'>{{ data.skill }}
+						<i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+					</li>
+				
+				</transition-group>
 			</ul>
-			<p>These are the skills that you possess.</p>
+			<p>These are the skills that you possess...</p>
 		</div>
   </div>
 </template>
@@ -22,7 +32,20 @@ export default {
 			{"skill": "Vue.js"},
 			{"skill": "Frontend Developer"}
 			]
-
+		}
+	},
+	methods: {
+		addSkill() {
+			this.$validator.validateAll().then((result) => {
+				if(result) {
+					this.skills.push({skill: this.skill})
+					this.skill = '';
+				}
+				console.log('not valid');
+			})
+		},
+		remove(id) {
+			this.skills.splice(id,1);
 		}
 	}
 }
@@ -31,6 +54,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
     background: #fff;
@@ -67,6 +92,30 @@ export default {
 		font-size: 1.3em;
 		background-color: #323333;
 		color: #687F7F;
+	}
+	.alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+	.alert-in-enter-active {
+  animation: bounce-in 1s;
+	}
+	.alert-in-leave-active {
+  	animation: bounce-in 1s reverse;
+	}
+	@keyframes bounce-in {
+  	0% {
+    	transform: scale(0);
+  	}
+  	50% {
+    	transform: scale(1.5);
+  	}
+  	100% {
+    	transform: scale(1);
+  	}
 	}
 
 </style>
